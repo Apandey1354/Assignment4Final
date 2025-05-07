b1051e3d77e62cc868b522fe1a157ad34d9dc214
Apandey1354 <apandey2@caldwell.edu>
2025-05-07 00:57:21 -0400

    Commit

---
+++ src/constants/breeds.js
@@ -0,0 +1,5 @@
+module.exports = {
+    bulldog: ['bullDog1.avif', 'bullDog2.webp', 'bullDog3.jpg'],
+    labrador: ['labrador1.avif', 'labrador2.jpg', 'labrador3.jpeg'],
+    poodle: ['poodle1.jpg', 'poodle2.webp', 'poodle3.webp']
+};
--- src/server.js
+++ src/server.js
@@ -27,6 +27,12 @@ app.get('/image/:breed', (req, res) => {
         res.status(404).json({ error: 'Breed not found' });
     }
 });
+
+// Add a wildcard route to handle all other GET requests
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
